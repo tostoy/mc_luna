e1bd49bc6aab9d1b448f775e27eaf9958194ea29
Huang Yi <huangyi@xianjincard.com>
2017-10-11 18:58:41 +0800

    add persistence feature

--- src/main/java/myudf/JsonParseTest.java
+++ src/main/java/myudf/JsonParseTest.java
@@ -8,16 +8,16 @@ public class JsonParseTest extends UDF {
     // TODO define parameters and return type, e.g:  public String evaluate(String a, String b)
     public String evaluate(String json) {
 
-        String path = "$.[?(@.deal_time > \"2017-02-02\")].sum";
+        String path = "$.contact_list.[?(@.call_in_cnt - @.call_in_cnt >= 0 )].phone_num";
         // String path="$.资信提示信息.[?(@.项目 == \"01.最近六个月内贷款申请信息\")]";
         try {
             Object ret = com.jayway.jsonpath.JsonPath.read(json, path);
             Gson gson = new Gson();
-//            path ="$.sum()";
+//            path ="$.strmax()";
 //            ret = com.jayway.jsonpath.JsonPath.read(gson.toJson(ret), path);
             return gson.toJson(ret);
         }catch(Exception e){
-            return null;
+            return e.getMessage();
         }
     }
 
